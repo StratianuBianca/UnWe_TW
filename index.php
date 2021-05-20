2e78c4b7861a21351b2752f6a0b9732493ed1787
StratianuBianca <bianca.stratianu@info.uaic.ro>
2021-05-20 22:57:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $JAVA_HOME = "\jdk-11.0.11";
 $PATH = "$JAVA_HOME/bin:".getenv('PATH');
 putenv("JAVA_HOME=$JAVA_HOME");
 putenv("PATH=$PATH");
-shell_exec("java  Main.java  2>&1");
+shell_exec("javac  Main.java  2>&1");
 $OUTPUT = shell_exec("java  Main 2>&1");
 echo $OUTPUT;
 ?>
