4042ec1ba9447354bfd81b4c997f5be26947a244
StratianuBianca <bianca.stratianu@info.uaic.ro>
2021-05-20 14:13:43 +0300

    update

--- Backend/src/main/java/edu/tw/contoller/AgeController.java
+++ Backend/src/main/java/edu/tw/contoller/AgeController.java
@@ -8,7 +8,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("https://database-unwe.herokuapp.com/api/v1/age")
+@Path("/api/v1/age")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class AgeController {
