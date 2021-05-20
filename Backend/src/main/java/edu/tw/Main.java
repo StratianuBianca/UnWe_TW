3f430d7550da1a06d8a67b16957721b530b9ffa6
StratianuBianca <bianca.stratianu@info.uaic.ro>
2021-05-20 22:14:35 +0300

    Deploy

--- Backend/src/main/java/edu/tw/Main.java
+++ Backend/src/main/java/edu/tw/Main.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.Response;
 import java.util.List;
 
 public class Main {
-    public Response parseURL (String url){
+    public Response parseURL (){
 
         AgeController ageController=new AgeController();
     //    List<Age> ageList=ageController.getAll();
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
 require_once 'principal/proiect.html';
-
+$output;
+exec("java -jar Backend-1.0-SNAPSHOT.jar ", $output);
+echo $output;
 ?>
