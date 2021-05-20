1857fb0e5ef4be43913ddd4843ec96c295476ea2
StratianuBianca <bianca.stratianu@info.uaic.ro>
2021-05-20 22:43:03 +0300

    try

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
 require_once 'principal/proiect.html';
-exec("java -cp  Backend/src/main/java/edu/tw/Main.java Hello 2>&1", $output);
+exec("java -cp  ./Backend/src/main/java/edu/tw/Main Hello 2>&1", $output);
 print_r($output);
 ?>
