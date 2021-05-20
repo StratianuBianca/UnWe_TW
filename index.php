5cc546db764c5635832f8e5af4098d41dd6f00a6
StratianuBianca <bianca.stratianu@info.uaic.ro>
2021-05-20 22:32:09 +0300

    change

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
 require_once 'principal/proiect.html';
-exec("java -jar Backend-1.0-SNAPSHOT.jar https://database-unwe.herokuapp.com 2>&1", $output);
+exec("java -cp  Backend/src/main/java/edu/tw Main 2>&1", $output);
 print_r($output);
 ?>
